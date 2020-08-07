c34aa49fd8f8fc1282643bc497ba720a515b7211
alangeek <alanchristiancreator@hotmail.com>
2020-08-07 18:38:38 -0300

    Add try/catch

--- server.js
+++ server.js
@@ -7,11 +7,16 @@ app.use(cors())
 
 app.get('/', async(req, res) => {
 
-    // response é a resposta do axios MAs eu tiro o data de dentro do response
-    const { data } = await axios('https://jsonplaceholder.typicode.com/users')
-    
-    return res.json(data)
-})
+    try {
+        // response é a resposta do axios MAs eu tiro o data de dentro do response
+        const { data } = await axios('https://jsonplaceholder.typicode.com/users')
+        
+        return res.json(data)    
+    } catch (error) {
+        console.log(error)
+    }
 
+    
+})
 
 app.listen('3000')
